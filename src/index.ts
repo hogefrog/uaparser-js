c9b9e28e4758faef4bd3ae02c3ccc4ec6a720eca
hogefrog <xatm092_04@hotmail.co.jp>
2017-09-28 17:18:40 +0900

    Stop reading file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import * as fs from 'fs'
-import * as path from 'path'
 import * as yaml from 'js-yaml'
 
 import clientinfo from './clientinfo'
@@ -7,31 +5,16 @@ import uaclass from './ua'
 import osclass from './os'
 import deviceclass from './device'
 
-const defaultFilePath = path.join('node_modules', 'uap-core', 'regexes.yaml')
-const defaultRegexes = yaml.load(fs.readFileSync(defaultFilePath, 'utf8'));
-
-export const parse = (useragent: string, filePath: string | undefined = defaultFilePath): clientinfo => {
-  let regexes = defaultRegexes;
-  if(filePath !== defaultFilePath) {
-    regexes = yaml.load(fs.readFileSync(filePath, 'utf8'));
-  }
+export const parse = (useragent: string, regexesData: string): clientinfo => {
+  let regexes = yaml.load(regexesData);
 
   let parseUA = uaclass.makeParser(regexes.user_agent_parsers);
-  let parseOS = osclass.makeParser(regexes.os_parsers);  
+  let parseOS = osclass.makeParser(regexes.os_parsers);
   let parseDevice = deviceclass.makeParser(regexes.device_parsers);
-  
+
   let ua = parseUA(useragent);
   let os = parseOS(useragent);
   let device = parseDevice(useragent);
 
   return new clientinfo(ua, os, device);
 }
-
-(() => {
-  if (require.main !== module) return;
-  if (process.argv.length < 3) return;
-  let ua = process.argv[2];
-  let regexesFilePath = process.argv[3];
-  let result = parse(ua, regexesFilePath);
-  console.log(result);
-})()
--- test/match.spec.ts
+++ test/match.spec.ts
@@ -8,6 +8,9 @@ import * as fs from 'fs';
 import { UATestCase, OSTestCase, DeviceTestCase } from './models.spec'
 
 const testsDir = path.join('node_modules', 'uap-core', 'tests/');
+const regexesFilePath = path.join('node_modules', 'uap-core', 'regexes.yaml');
+const regexesData = fs.readFileSync(regexesFilePath, 'utf8');
+
 const tests = {
   'os': path.join(testsDir + 'test_os.yaml'),
   'device': path.join(testsDir + 'test_device.yaml'),
@@ -19,7 +22,7 @@ describe('UAP Core Test Resource Matching', () => {
     const fixtures = <OSTestCase[]>(parseYaml(tests.os)).test_cases;
     fixtures.forEach(element => {
       const testcase = new OSTestCase(element)
-      const result = UAParser.parse(testcase.user_agent_string);
+      const result = UAParser.parse(testcase.user_agent_string, regexesData);
       assert.equal(result.os.family, testcase.family);
       assert.equal(result.os.major, testcase.major);
       assert.equal(result.os.minor, testcase.minor);
@@ -32,7 +35,7 @@ describe('UAP Core Test Resource Matching', () => {
     const fixtures = <DeviceTestCase[]>(parseYaml(tests.device)).test_cases;
     fixtures.forEach(element => {
       const testcase = new DeviceTestCase(element)
-      const result = UAParser.parse(testcase.user_agent_string);
+      const result = UAParser.parse(testcase.user_agent_string, regexesData);
       assert.equal(result.device.family, testcase.family);
       assert.equal(result.device.brand, testcase.brand);
       assert.equal(result.device.model, testcase.model);
@@ -43,7 +46,7 @@ describe('UAP Core Test Resource Matching', () => {
     const fixtures = <UATestCase[]>(parseYaml(tests.ua)).test_cases;
     fixtures.forEach(element => {
       const testcase = new UATestCase(element)
-      const result = UAParser.parse(testcase.user_agent_string);
+      const result = UAParser.parse(testcase.user_agent_string, regexesData);
       assert.equal(result.ua.family, testcase.family);
       assert.equal(result.ua.major, testcase.major);
       assert.equal(result.ua.minor, testcase.minor);
